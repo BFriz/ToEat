390a169d30c9152989264c6159caf814612790ab
Ben Frisby <bfrisb@gmail.com>
2015-06-15 18:13:27 +0100

    can access the homepage working on getting the food to show.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,17 @@
 $function(){
-
-
+// getting the food and stuff
+  Food.all();
   
+}
+
+function View() {};
+View.init = function() {
+  // food form submit event listener
+  $("#food-form").on("submit", function(e){
+    // stop page reload
+    e.preventDefault();
+    // format form data into a query string
+    var foodParams = $(this).serialize();
+    Food.create(foodParams);
+  });
 }
